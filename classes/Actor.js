0896a970dd484853f07f4152610155e4bd413775
Alex <alejandro_liu@hotmail.com>
2021-10-17 20:34:39 +0000

    added comments

--- classes/Actor.js
+++ classes/Actor.js
@@ -1,5 +1,6 @@
 
 class Actor {
+  // Base class of objects that move accross the playing field
   constructor(x,y,w,h) {
     this.xpos = x;
     this.ypos= y;
@@ -13,13 +14,26 @@ class Actor {
     this.ypos += this.yspeed;
   }
   draw() {}
+  // Check if this coordinate is within the bounding rectangle
   isHit(x,y) {
     return ((this.xpos < x && x < this.xpos + this.width) && (this.ypos < y && y < this.ypos + this.height));
   }
+  // Check if this rectangle is colliding with a rectangle
   isRectHit(x,y,w,h) {
     return (this.isHit(x,y) || this.isHit(x+w,y) || this.isHit(x,y+h) || this.isHit(x+w,y+h));
   }
+  // Check a collision between actors
   isCrash(actor) {
     return this.isRectHit(actor.xpos, actor.ypos, actor,width, actor.height);
   }
+  isOffScreen() {
+    if (this.ypos < 0 || this.ypos > CANVAS_HEIGHT) {
+      return true;
+    }
+    if (this.xpos < 0 || this.xpos > CANVAS_WIDTH) {
+      return true;
+    }
+    return false;
+  }
+
 }
--- const.js
+++ const.js
@@ -1,6 +1,6 @@
-
 const CANVAS_HEIGHT = 640;
 const CANVAS_WIDTH = 480;
 
+// Constants representing the type of death of the player
 const INVADER_CRASH = 1;
 const BULLET_HIT = 2;
